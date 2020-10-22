8a2348f3c0396225246702568e9215ac3b4961d2
milo0606 <emiliosanchez999@gmail.com>
2020-10-21 23:18:44 -0400

    Se agrego contenedor de fondo

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,23 @@ class MyApp extends StatelessWidget {
         drawer: Drawer(),
         appBar: AppBar(
           backgroundColor: Colors.black87,
-          elevation: 6,
+          elevation: 20,
           centerTitle: true ,
           title: Text("Gaming Nation Store",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25)),
 
         ),
         body: Center(
           child: Container(
-            child: Text('Hello World'),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(70),topRight: Radius.circular(70)),
+              shape: BoxShape.rectangle,
+              color: Colors.black
+            ),
+            margin: EdgeInsets.only(top: 300),
+
+
           ),
-        ),
+        )
       ),
     );
   }
